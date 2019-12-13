240202f0f58115d9a681d1488cc9af0a823052b0
Max Zakzook <mzakzook@me.com>
2019-12-13 11:51:46 -0800

    Confirm successful new user post to database

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::AuthController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: user_login_params[:username])
+    @user = User.find_by(email: user_login_params[:email])
     #User#authenticate comes from BCrypt
     if @user && @user.authenticate(user_login_params[:password])
       # encode token comes from ApplicationController
@@ -17,7 +17,7 @@ class Api::V1::AuthController < ApplicationController
       token = encode_token({ user_id: @user.id })
       render json: { user: UserSerializer.new(@user), token: token }, status: :accepted
     else
-      render json: { error: 'Invalid username or password' }, status: :unauthorized
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
     end
   end
  
@@ -27,6 +27,6 @@ class Api::V1::AuthController < ApplicationController
  
   def user_login_params
     # params { user: {username: 'Chandler Bing', password: 'hi' } }
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:email, :password)
   end
 end
---
+++ db/migrate/20191213194649_change_password_attribute.rb
@@ -0,0 +1,5 @@
+class ChangePasswordAttribute < ActiveRecord::Migration[6.0]
+  def change
+    rename_column :users, :password, :password_digest
+  end
+end
